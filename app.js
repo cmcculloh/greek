3922cd4211520fd9ba4b242ec4b0b2b3270f9fc9
Christopher McCulloh <cmcculloh@cmcculloh-ltm.internal.salesforce.com>
2019-09-24 15:37:43 -0400

    trying to get static files to serve

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 var static = require('node-static');
-var file = new static.Server();require('http').createServer(function(request, response) {
-  request.addListener('end', function() {
-    file.serve(request, response);
-  }).resume();
+
+//
+// Create a node-static server instance to serve the './public' folder
+//
+var file = new static.Server('.');
+
+require('http').createServer(function (request, response) {
+    request.addListener('end', function () {
+        //
+        // Serve files!
+        //
+        file.serve(request, response);
+    }).resume();
 }).listen(process.env.PORT || 3000);
