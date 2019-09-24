cc971846f8fb6620574dbd95fca3d0107297c0af
Christopher McCulloh <cmcculloh@cmcculloh-ltm.internal.salesforce.com>
2019-09-24 15:43:43 -0400

    use express

--- app.js
+++ app.js
@@ -1,15 +1,11 @@
-var static = require('node-static');
+var express = require('express');
+var path = require('path');
+var app = express();
 
-//
-// Create a node-static server instance to serve the './public' folder
-//
-var file = new static.Server('.');
+app.use('/', express.static(path.join(__dirname, 'public')));
 
-require('http').createServer(function (request, response) {
-    request.addListener('end', function () {
-        //
-        // Serve files!
-        //
-        file.serve(request, response);
-    }).resume();
-}).listen(process.env.PORT || 3000);
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+app.listen(port);
